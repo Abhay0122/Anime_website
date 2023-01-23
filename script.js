603930289e8354e5f99cfb8fae7be4c60d4f7a81
Abhay0122 <parmarabhay2201@gmail.com>
2023-01-24 01:10:57 +0530

    another commit

--- script.js
+++ script.js
@@ -60,15 +60,58 @@ function show() {
 
     arr.forEach((elem) => {
         clutter += `<div class="page_3_cards">
-                        <img src="${elem.img}" alt="">
-                        <div class="page_3_cards_txts">
-                            <h2>${elem.name}</h2>
-                            <span>${elem.episode}</span>
-                        </div>
-                    </div>`
+        <img src="${elem.img}" alt="">
+        <div class="page_3_cards_txts">
+            <h2>${elem.name}</h2>
+            <span>${elem.episode}</span>
+        </div>
+    </div>`
     })
     document.querySelector('.page_3-main-card_container').innerHTML = clutter;
 };
 
 show();
 
+
+// card-page_4
+
+var arr2 = [
+    { img: "/images/Onepiece.jpg", name: "One Piece", rank: "Rank-01" },
+    { img: "/images/narutoshhipuden.jpg", name: "Naruto: Shippuden", rank: "Rank-02" },
+    { img: "/images/Bleach.png", name: "Bleach", rank: "Rank-03" },
+    { img: "/images/jujutsu-kaisen-bg.jpg", name: "Jujutsu kaisen", rank: "Rank-04" },
+    { img: "/images/one-punch-man-saitama-wgbs1b3mcd3l28tq.jpg", name: "One Punch", rank: "Rank-05" },
+    { img: "/images/AOT.png", name: "Attack On Titan", rank: "Rank-06" },
+    { img: "/images/anos.jpg", name: "Demon Academy S-02", rank: "Rank-07" },
+    { img: "/images/Boruto.png", name: "Boruto", rank: "Rank-08" },
+    { img: "/images/dbz.jpg", name: "Dragon Ball Z", rank: "Rank-09" },
+    { img: "/images/Spy Kyoushitsu.png", name: "Spy Kyoushitsu", rank: "Rank-10" },
+    { img: "/images/Nier_Automata_Ver1.1a_kv.jpg", name: "NieR:Automata Ver1.1a", rank: "Rank-11" },
+    { img: "/images/Bungou Stray Dogs 4th Season.jpg", name: "Bungou Stray Dogs S-4th", rank: "Rank-12" },
+    { img: "/images/Tokyo Revengers.jpg", name: "Tokyo Revengers", rank: "Rank-13" },
+    { img: "/images/blue lock.jpg", name: "Blue Lock", rank: "Rank-14" },
+    { img: "/images/Detective Conan.jpg", name: "Detective Conan", rank: "Rank-15" },
+    { img: "/images/deathnote.jpg", name: "Death Note", rank: "Rank-16" },
+    { img: "/images/Dungeon.png", name: "Dungeon", rank: "Rank-17" },
+    { img: "/images/Boku no Hero Academia 6th Season.jpg", name: "Boku no Hero Academia S-6th", rank: "Rank-18" },
+    { img: "/images/Kyokou Suiri Season 2.jpg", name: "Kyokou Suiri Season 2", rank: "Rank-19" },
+    { img: "/images/Horimiya.jpg", name: "Horimiya", rank: "Rank-20" },
+]
+
+
+function showCard() {
+    var clutterCard = "";
+
+    arr2.forEach((element) => {
+        clutterCard += `<div class="page_4_card">
+        <img src="${element.img}" alt="">
+        <div class="page_4_card_txts">
+            <h2>${element.name}</h2>
+            <span>${element.rank}</span>
+        </div>
+    </div>`
+    })
+    document.querySelector('.page_4_cards_container').innerHTML = clutterCard;
+};
+
+showCard();
